e1dff65f8216f9bec7631dd905b55e26fd3e803e
emelecio966 <143520582+emelecio966@users.noreply.github.com>
2024-05-02 14:34:47 +0000

    emerald

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,6 +1,9 @@
 class ZebraController < ApplicationController 
   def giraffe
-     
-    render({ :template => "game_templates/play_stones"}) 
+    render({ :template => "game_templates/play_stones" }) 
   end
+end 
+
+def rules
+   render({ :template => "game_templates/rules" })
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/stones", { :controller => "zebra", :action => "giraffe"}) 
   
-  
+  get("/rules", { :controller => "zebra", :action => "rules"})
   
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
